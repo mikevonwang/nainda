7e7db812e89ce506424de015f9bea9ac0fdde631
Mike Wang <mikevonwang@gmail.com>
2018-02-15 21:00:50 -0600

    add documentation

--- src/nainda.js
+++ src/nainda.js
@@ -1,6 +1,6 @@
 function nainda(string_1, string_2, t) {
-  let a;
-  let b;
+  let a; // shorter string
+  let b; // longer string
   if (string_2.length < string_1.length) {
     a = string_2;
     b = string_1;
@@ -11,45 +11,44 @@ function nainda(string_1, string_2, t) {
   }
   const a_len = a.length;
   const b_len = b.length;
-  const n = b_len - a_len + 1 + (2 * t);
-  let y0 = -t;
+  const n = b_len - a_len + 1 + (2 * t); // total number of alignments
+  let y0 = -t; // first index of b to be compared for this alignment
   let lowest_score = Infinity;
   for (let i=0; i<n; i++) {
-    let x = 0;
-    let y = y0;
-    let s = 0;
-    let skips = 0;
-    while (y<a_len+y0+skips) {
-      if (a[x] === b[y]) {
+    let x = 0; // index of a currently being compraed
+    let y = y0; // index of b currently being compared
+    let s = 0; // score for this alignment
+    let skips = 0; // skip count for this alignment
+    while (y < a_len + y0 + skips) { // go until you run out of characters in a
+      if (a[x] === b[y]) { //
         x++;
         y++;
       }
       else {
-
-        let z = 1;
+        let z = 1; // add-on to y
         let match_found = false;
         while (z <= t) {
-          if (a[x] === b[y+z]) {
-            y = y + z + 1;
-            x++;
-            s += z;
-            skips += 1;
+          if (a[x] === b[y+z]) { // if a skip of z results in a match
+            y = y + z + 1; // set new y
+            x++; // set new x
+            s += z; // increase score for this alignment by z
+            skips += z; // increase skip count for this alignment by z
             match_found = true;
             break;
           }
           z++;
         }
-        if (match_found === false) {
-          y++;
-          x++;
-          s++;
+        if (match_found === false) { // the maximum number of skips has occurred with no match
+          y++; // move to next y
+          x++; // move to next x
+          s++; // increase score for this alignment by 1
         }
       }
     }
     if (s < lowest_score) {
       lowest_score = s;
     }
-    y0++;
+    y0++; // shift alignment by 1
   }
   return (lowest_score <= t);
 };
